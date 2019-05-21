0e44c93061f3ee2e86ef3f24f2af675251a18cd1
makbari.it <makbari.it@outlook.com>
2019-05-21 21:20:09 +0430

    change connection

--- WebApplication6/Data/BlogContext.cs
+++ WebApplication6/Data/BlogContext.cs
@@ -10,7 +10,10 @@ namespace WebApplication6.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             //=> optionsBuilder.UseNpgsql("Host=raja.db.elephantsql.com;Database=mnoyzxti;Username=mnoyzxti;Password=zMykjICPmmoO7zIpOIs-YIZbqDCHOvV-");
-            => optionsBuilder.UseNpgsql("Host=ec2-54-235-167-210.compute-1.amazonaws.com;Database=d5b4bh7mutujv8;Username=nrerdmgqtzusom;Password=9c98b93835d17c8c7258aeb27c4224a80ce8abec1f7287c77a308624ba4c6c2f");
+            => optionsBuilder.UseNpgsql("Host=ec2-54-83-192-245.compute-1.amazonaws.com;" +
+                "Database=d4eenhcdpd9jt1;" +
+                "Username=amgzhqwusciomu;" +
+                "Password=20f0f9bc906288da640cd19b72b453a35bb743484e08cf817c95998bcc515501");
 
 
 
